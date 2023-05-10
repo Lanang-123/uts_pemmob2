e1149c0b85b0265e08fcff8d5d175337016292c7
DarkGlitc <hendi123wijaya@gmail.com>
2023-05-10 21:47:56 +0800

    progress gigs detail update sebelum uts

--- lib/components/BannerGig.dart
+++ lib/components/BannerGig.dart
@@ -1,9 +1,10 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:yoii/data/bannerHome.dart';
+import 'package:yoii/theme.dart';
 
 class BannerGig extends StatefulWidget {
-  final Map<String, dynamic> images;
+  final List<String> images;
   const BannerGig({super.key, required this.images});
 
   @override
@@ -20,67 +21,66 @@ class _BannerGigState extends State<BannerGig> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     final data = widget.images;
-    print(data);
 
-    return Stack(children: [
-      InkWell(
-        onTap: () {
-          print(currentIndex);
-        },
-        child: CarouselSlider(
-            items: bannerHome
-                .map((bh) => ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: SizedBox(
-                          width: width,
-                          height: 141,
-                          child: Stack(
-                            children: [
-                              SizedBox(
-                                width: double.maxFinite,
-                                child: Image.asset(
-                                  bh['img_path'],
-                                  fit: BoxFit.cover,
-                                ),
+    return Container(
+      margin: const EdgeInsets.only(top: 15),
+      height: 250,
+      child: Stack(children: [
+        InkWell(
+          onTap: () {
+            print(currentIndex);
+          },
+          child: CarouselSlider(
+              items: data
+                  .map((bh) => ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: SizedBox(
+                            width: width,
+                            child: SizedBox(
+                              width: double.maxFinite,
+                              child: Image.asset(
+                                bh,
+                                fit: BoxFit.cover,
                               ),
-                            ],
-                          )),
-                    ))
-                .toList(),
-            carouselController: carouselController,
-            options: CarouselOptions(
-                autoPlay: true,
-                aspectRatio: 2.5,
-                viewportFraction: 1,
-                scrollPhysics: const BouncingScrollPhysics(),
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    currentIndex = index;
-                  });
-                })),
-      ),
-      Positioned(
-        bottom: 10,
-        left: 10,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: bannerHome.asMap().entries.map((entry) {
-            return GestureDetector(
-              onTap: () => carouselController.animateToPage(entry.key),
-              child: Container(
-                width: currentIndex == entry.key ? 17 : 7,
-                height: 7.0,
-                margin: const EdgeInsets.symmetric(horizontal: 3.0),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: currentIndex == entry.key
-                        ? Colors.white
-                        : Colors.white.withOpacity(0.4)),
-              ),
-            );
-          }).toList(),
+                            )),
+                      ))
+                  .toList(),
+              carouselController: carouselController,
+              options: CarouselOptions(
+                  autoPlay: true,
+                  aspectRatio: 2,
+                  viewportFraction: 1,
+                  scrollPhysics: const BouncingScrollPhysics(),
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  })),
         ),
-      )
-    ]);
+        Positioned(
+          top: 200,
+          left: 0,
+          right: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: data.asMap().entries.map((entry) {
+              return GestureDetector(
+                onTap: () => carouselController.animateToPage(entry.key),
+                child: Container(
+                  width: currentIndex == entry.key ? 17 : 7,
+                  height: 7.0,
+                  margin: const EdgeInsets.symmetric(horizontal: 3.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: currentIndex == entry.key
+                          ? ungu1
+                          : Colors.grey.withOpacity(0.4)),
+                ),
+              );
+            }).toList(),
+          ),
+        )
+      ]),
+    );
   }
 }
--- lib/process/GigDetail.dart
+++ lib/process/GigDetail.dart
@@ -11,18 +11,29 @@ class GigDetail extends StatefulWidget {
 }
 
 class _GigDetailState extends State<GigDetail> {
+  bool isChoose1 = false;
+  bool isChoose2 = false;
+  bool isChoose3 = false;
+
+  List<String> dataPaket = [
+    'Mendapatkan 12 file foto Edited',
+    'Color Grading dan Curve edting',
+    'Include Retouching kulit',
+    'Include Retouching background'
+  ];
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
     final apBar = AppBar(
-      iconTheme: const IconThemeData(color: Colors.black),
+      iconTheme: const IconThemeData(color: Colors.white),
       elevation: 2,
-      backgroundColor: Colors.white,
+      backgroundColor: ungu1,
       title: Text(
         'Detail Gig',
-        style: semibold.copyWith(fontSize: 22, color: Colors.black),
+        style: semibold.copyWith(fontSize: 22, color: Colors.white),
       ),
       centerTitle: true,
     );
@@ -33,8 +44,10 @@ class _GigDetailState extends State<GigDetail> {
       appBar: apBar,
       body: SingleChildScrollView(
           child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        padding:
+            const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 170),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               data['desc'],
@@ -81,9 +94,303 @@ class _GigDetailState extends State<GigDetail> {
             BannerGig(
               images: data['gallery'],
             ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isChoose1 = true;
+                      isChoose2 = false;
+                      isChoose3 = false;
+                    });
+                  },
+                  child: Container(
+                    width: 114,
+                    height: 49,
+                    decoration: BoxDecoration(
+                      color: isChoose1 ? ungu1 : Colors.grey,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Paket 1",
+                        style: semibold.copyWith(
+                            fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isChoose1 = false;
+                      isChoose2 = true;
+                      isChoose3 = false;
+                    });
+                  },
+                  child: Container(
+                    width: 114,
+                    height: 49,
+                    decoration: BoxDecoration(
+                      color: isChoose2 ? ungu1 : Colors.grey,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Paket 2",
+                        style: semibold.copyWith(
+                            fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isChoose1 = false;
+                      isChoose2 = false;
+                      isChoose3 = true;
+                    });
+                  },
+                  child: Container(
+                    width: 114,
+                    height: 49,
+                    decoration: BoxDecoration(
+                      color: isChoose3 ? ungu1 : Colors.grey,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Paket 3",
+                        style: semibold.copyWith(
+                            fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: width,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                elevation: 3,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Stack(
+                          children: [
+                            Text(
+                              "Rp${data['price']}",
+                              style: bold.copyWith(fontSize: 32),
+                              textAlign: TextAlign.center,
+                            ),
+                            Positioned(
+                              bottom: 0, // atur posisi bottom border
+                              left: 0,
+                              right: 0,
+                              child: Container(
+                                height: 2,
+                                color: ungu1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Center(
+                        child: Text(
+                          'Yang Include dalam Paket',
+                          style: semibold.copyWith(
+                              fontSize: 14,
+                              color: Colors.grey.withOpacity(0.6)),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Image.asset(
+                                "assets/icons/revisiGigs.png",
+                                width: 30,
+                                height: 30,
+                              ),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "1 Hari \n penyelesaian",
+                                style: semibold.copyWith(
+                                    fontSize: 15, color: Colors.black),
+                                textAlign: TextAlign.center,
+                              )
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Image.asset(
+                                "assets/icons/timeGigs.png",
+                                width: 30,
+                                height: 30,
+                              ),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "Revisi tidak \n terbatas",
+                                style: semibold.copyWith(
+                                    fontSize: 15, color: Colors.black),
+                                textAlign: TextAlign.center,
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                          height: 140,
+                          child: ListView.builder(
+                              itemCount: dataPaket.length,
+                              itemBuilder: ((context, index) {
+                                return ListTile(
+                                  leading: Icon(
+                                    Icons.check,
+                                    color: ungu1,
+                                    size: 25,
+                                  ),
+                                  title: Text(
+                                    dataPaket[index],
+                                    style: medium.copyWith(
+                                        fontSize: 14,
+                                        color: Colors.grey.withOpacity(0.5)),
+                                  ),
+                                );
+                              }),
+                              itemExtent: 30))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Detail Gig",
+              style: semibold.copyWith(fontSize: 26),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              "Hai Selamat Datang di Gig Saya,Apakah Anda mencari layanan Foto Profesional yang dapat dikirimkan dalam waktu 24 jam atau kurang",
+              style: regular.copyWith(fontSize: 16),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Pengeditan Foto itu rumit, jadi silakan hubungi saya dengan deskripsi Anda tentang apa yang Anda inginkan. Wedding, Foto tema old, beauty atau yang lainnya yang dapat mendeskripsikan secara detail tema foto tersebut Dan semua jenis pengeditan Video dapat dilakukan",
+              style: regular.copyWith(fontSize: 16),
+            ),
           ],
         ),
       )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
+        height: 150,
+        width: width,
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(
+              color: Colors.grey.withOpacity(0.6),
+              blurRadius: 3,
+              spreadRadius: 3,
+              offset: Offset(2, 2))
+        ]),
+        child: Center(
+          child: Column(
+            children: [
+              FilledButton(
+                  style: ButtonStyle(
+                      padding: MaterialStatePropertyAll(
+                          EdgeInsets.symmetric(vertical: 8)),
+                      backgroundColor: const MaterialStatePropertyAll(ungu1),
+                      shape: MaterialStatePropertyAll(ContinuousRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)))),
+                  onPressed: () {},
+                  child: Center(
+                    child: Text(
+                      "Lanjutkan",
+                      style: bold.copyWith(fontSize: 20, color: Colors.white),
+                    ),
+                  )),
+              const SizedBox(
+                height: 8,
+              ),
+              Container(
+                width: width,
+                child: OutlinedButton(
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(
+                        EdgeInsets.symmetric(vertical: 10)),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    side: MaterialStateProperty.all(
+                      BorderSide(
+                        width: 2,
+                        color: ungu1,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/icons/chat.png',
+                        width: 22,
+                        height: 22,
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        "Hubungi Penjual",
+                        style: bold.copyWith(fontSize: 20, color: ungu2),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
